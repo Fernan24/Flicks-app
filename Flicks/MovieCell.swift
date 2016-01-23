6e606c8e594295fb7a912570ef313ae4cb975466
Fernando Rodriguez <fernando.rodriguez10@upr.edu>
2016-01-23 13:21:58 -0400

    im having toruble adding the poster to the image view, xcode does not recognize the setImageWithUrl method.

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Fernando Rodríguez on 1/23/16.
+//  Copyright © 2016 Fernando Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+   
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
